94d1a7a384fd6316ad60ddad8534488404724caf
Niiik27 <niiik404@gmail.com>
2023-09-28 18:10:04 +0400

    Пока что что то сломалось пока детал события и особенно их детали

--- events/views.py
+++ events/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from APP_NAMES import APP_NAMES, VERBOSE_APP_NAMES
 app_name = APP_NAMES.EVENTS
 verbose_name = VERBOSE_APP_NAMES.EVENTS
-
+from .models import Event
 def eventsView(request):
-    return render(request, template_name=f'./{app_name}/{app_name}.html', context={'page_name':verbose_name,'page_style':app_name})
+    events = Event.objects.all()
+    return render(request, template_name=f'./{app_name}/{app_name}.html', context={'events':events,'page_name':verbose_name,'page_style':app_name})
+
+def detailView(request, event_id):
+    event = get_object_or_404(Event, pk = event_id)
+    return render(request, 'detail/detail.html',{'event':event, 'page_name':event.title,'page_style':app_name})
