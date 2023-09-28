9fe1caa4dd302eb718671ac56501be900b11e120
Niiik27 <niiik404@gmail.com>
2023-09-28 19:19:21 +0400

    Сломанные события

--- events/models.py
+++ events/models.py
@@ -1,15 +1,19 @@
 from django.db import models
-from . views import app_name,verbose_name
+# from .views import app_name, verbose_app_name
+
+
 # Таблица мероприятий
-class Event(models.Model):
-    title = models.CharField('Заголовок',max_length=15)  # Ограниченная строка
-    desc= models.TextField('Описание')  # Ограниченная строка
-    image= models.ImageField('Изображение',upload_to='blog/image')  # строка по изображению (отдельный тип данных)
-    date = models.DateField('Дата')  # Дата
-    url = models.URLField('Подробнее о событии',blank=True)  # Ссылка
-    def __str__(self):
-        return f"{self.title} | {self.date}"
-    class Meta:
-        verbose_name = app_name
-        verbose_name_plural = verbose_name
+class MyEvent(models.Model):
+    pass
+    # title = models.CharField(verbose_name='Заголовок', max_length=15)  # Ограниченная строка
+    # desc = models.TextField(verbose_name='Описание')  # Ограниченная строка
+    # image = models.ImageField(verbose_name='Изображение', upload_to='blog/image')  # строка по изображению (отдельный тип данных)
+    # date = models.DateField(verbose_name='Дата')  # Дата
+    # url = models.URLField('Подробнее о событии', blank=True)  # Ссылка
+    # class Meta:
+    #     verbose_name = app_name
+    #     verbose_name_plural = verbose_app_name
+    # def __str__(self):
+    #     return f"{self.title} | {self.date}"
+
 
--- events/views.py
+++ events/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from APP_NAMES import APP_NAMES, VERBOSE_APP_NAMES
+from .models import MyEvent
 app_name = APP_NAMES.EVENTS
-verbose_name = VERBOSE_APP_NAMES.EVENTS
-from .models import Event
-def eventsView(request):
-    events = Event.objects.all()
-    return render(request, template_name=f'./{app_name}/{app_name}.html', context={'events':events,'page_name':verbose_name,'page_style':app_name})
+verbose_app_name = VERBOSE_APP_NAMES.EVENTS
 
-def detailView(request, event_id):
-    event = get_object_or_404(Event, pk = event_id)
-    return render(request, 'detail/detail.html',{'event':event, 'page_name':event.title,'page_style':app_name})
+def eventsView(request):
+    # events = MyEvent
+    return render(request, template_name=f'./{app_name}/{app_name}.html', context={'events':'events','page_name':verbose_app_name, 'page_style':app_name})
+
+# def detailView(request, event_id):
+#     event = get_object_or_404(MyEvent, pk = event_id)
+#     return render(request, 'detail/detail.html',{'event':event, 'page_name':event.title,'page_style':app_name})
