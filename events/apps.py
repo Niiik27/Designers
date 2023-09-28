4a5f5b5927e5d105c655488d0f3eb717863b0340
Niiik27 <niiik404@gmail.com>
2023-09-28 19:27:02 +0400

    Сломанные события

--- events/apps.py
+++ events/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
-from . views import app_name,verbose_app_name
+# from . views import app_name,verbose_app_name
 
 class MyEventConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = app_name
-    verbose_name = verbose_app_name
+    name = 'events'
+    verbose_name = 'События'
