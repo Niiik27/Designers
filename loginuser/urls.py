840c65ce7b1775a62afaf74e90909bc936384346
Niiik27 <niiik404@gmail.com>
2023-10-04 18:17:51 +0400

    Обработка данных пользователя. пока в глубоком процессе

--- loginuser/urls.py
+++ loginuser/urls.py
@@ -1,5 +1,5 @@
 
-from django.urls import path
+from django.urls import path, include
 from . import views
 from APP_NAMES import APP_NAMES
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.loginuserView, name=APP_NAMES.LOGIN_USER),
     path(f'{APP_NAMES.LOGOUT_USER}/', views.logoutuserView, name=APP_NAMES.LOGOUT_USER),
     path(f'{APP_NAMES.REG_USER}/', views.reguserView, name=APP_NAMES.REG_USER),
+    path(f'{APP_NAMES.USER_PROFILE}/', include(f'{APP_NAMES.USER_PROFILE}.urls')),
 ]
