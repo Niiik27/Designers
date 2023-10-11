4c549f9ba201ca2acca150f67f72181e99c81baf
Niiik27 <niiik404@gmail.com>
2023-10-11 16:36:19 +0400

    почти сделал портфолио

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -2,6 +2,11 @@ from django import forms
 from .models import Artwork
 
 class ArtworkForm(forms.ModelForm):
+    artwork = forms.FileField(widget=forms.FileInput(attrs={'class': 'button'}))
+    title = forms.CharField(widget=forms.TextInput(attrs={'class': 'txt-input'}))
+    desc = forms.CharField(widget=forms.TextInput(attrs={'class': 'txt-input'}))
+    date = forms.CharField(widget=forms.TextInput(attrs={'class': 'txt-input'}))
+    url = forms.CharField(widget=forms.TextInput(attrs={'class': 'txt-input'}))
     class Meta:
         model = Artwork
         fields = [
